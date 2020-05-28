e8177172718ae14a9637209f67f630cc265d697f
carminelentisco <carminelentisco@gmail.com>
2020-05-28 21:55:12 +0200

    Aggiornamento index.php

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
-// Home server
-include __DIR__ . '/partials/home/server.php'; 
+/********************************************************
+  
+*                         INDEX.PHP                     *
+ 
+********************************************************/
 
-// Head page code
-include __DIR__ . '/partials/templates/head.php';
+include __DIR__ . '/partials/home/server.php';      // Home server -- Collegamento alla logica server impostata  
+include __DIR__ . '/partials/templates/head.php';   // Head page code -- Template HTML 
 
 ?>
 
 <main class="container">
     <div class="row">
         <div class="col-12">
+            
             <header>
                 <h1 class="text-primary">Hotel Rooms</h1>                    
             </header>
@@ -27,26 +31,19 @@ include __DIR__ . '/partials/templates/head.php';
                 </thead>
 
                 <tbody>
-                    <?php 
+                    <?php
                     if (! empty($rooms)) {
                         foreach ($rooms as $room) { ?>
                             <tr>
                                 <td><?php echo $room['id']; ?></td>
                                 <td><?php echo $room['room_number']; ?></td>
                                 <td><?php echo $room['floor']; ?></td>
-                                <td>
-                                    <a class="text-success" href="./show.php?id=<?php echo $room['id']; ?>">view</a>
-                                </td>
-                                <td>
-                                    <a class="text-primary" href="">update</a>
-                                </td>
-                                <td>
-                                    <a href="" class="text-danger">delete</a>
-                                </td>
+                                <td><a class="text-success" href="./show.php?id=<?php echo $room['id']; ?>">view</a></td>
+                                <td><a class="text-primary" href="">update</a></td>
+                                <td><a href="" class="text-danger">delete</a></td>
                             </tr>
-                        <?php }
-                    }
-                    ?>
+                    <?php }
+                    }?>
                 </tbody>
             </table>
 
@@ -55,5 +52,5 @@ include __DIR__ . '/partials/templates/head.php';
 </main>
 
 <?php 
-    include __DIR__ . '/partials/templates/footer.php';
+    include __DIR__ . '/partials/templates/footer.php'; // Footer page -- Template HTML { Script js compresi }
 ?>
