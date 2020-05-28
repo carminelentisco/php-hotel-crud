65e2a07dc8204556a478cf06ef22746e8e3aa62d
carminelentisco <carminelentisco@gmail.com>
2020-05-28 21:54:54 +0200

    Aggiornamento Database

--- partials/database.php
+++ partials/database.php
@@ -1,11 +1,36 @@
 <?php 
+/********************************************************
+  
+*                      DATABASE.PHP                     *
+ 
+********************************************************/
 
-include __DIR__ . '/../env.php';
+include __DIR__ . '/../env.php';    // Accesso al file con credenziali
 
-// Connessione al database
-$conn = new mysqli($server_name,$username,$password,$db_name);
+/**
+ * Nella stringa seguente avviene la connessione al server.
+ * Si inizializza una variablie, dandogli il costrutto { new } ed il metodo { mysqli }
+ * 
+ * mysqli() è un metodo che accetta i seguenti parametri in questa precisa sequenza.
+ *  1 - Nome del server 
+ *  2 - Nome Utente
+ *  3 - Password
+ *  4 - Nome assegnato al database
+ * 
+ * In questo caso i file sono presenti in un secondo file non versionato con GIT
+ *  
+ */
 
-// Controllo della connessione
+$conn = new mysqli($server_name,$username,$password,$db_name);  
+
+/**  
+ *  Controllo della connessione:
+ * 
+ *  Nel seguente if viene controlla l'effettiva connessione al server,
+ *  tramite l'elemento dell oggetto connect_error
+ *   
+ */
+
 if ($conn && $conn->connect_error) {
     die('Si è verificato un errore di connessione con il db');
 }
