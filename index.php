183caa71a1c3f13de5e98dd844ba6fbf32a3e7a3
carminelentisco <carminelentisco@gmail.com>
2020-05-28 17:11:12 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+// Home server
+include __DIR__ . '/partials/home/server.php'; 
+
+// Head page code
+include __DIR__ . '/partials/templates/head.php';
+
+?>
+
+<main class="container">
+    <div class="row">
+        <div class="col-12">
+            <header>
+                <h1 class="text-primary">Hotel Rooms</h1>                    
+            </header>
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>Number</th>
+                        <th>Floor</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php 
+                    if (! empty($rooms)) {
+                        foreach ($rooms as $room) { ?>
+                            <tr>
+                                <td><?php echo $room['id']; ?></td>
+                                <td><?php echo $room['room_number']; ?></td>
+                                <td><?php echo $room['floor']; ?></td>
+                                <td>
+                                    <a class="text-success" href="./show.php?id=<?php echo $room['id']; ?>">view</a>
+                                </td>
+                                <td>
+                                    <a class="text-primary" href="">update</a>
+                                </td>
+                                <td>
+                                    <a href="" class="text-danger">delete</a>
+                                </td>
+                            </tr>
+                        <?php }
+                    }
+                    ?>
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+</main>
+
+<?php 
+    include __DIR__ . '/partials/templates/footer.php';
+?>
