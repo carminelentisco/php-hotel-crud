73c5e9fd82c859dd6cf8f17c6703567b69862e1e
carminelentisco <carminelentisco@gmail.com>
2020-06-03 15:42:52 +0200

    Logica precedente sostituita con una funzione

--- partials/home/server.php
+++ partials/home/server.php
@@ -4,36 +4,7 @@
 *              SERVER.PHP __di__ HOME.PHP               *
  
 ********************************************************/
+include __DIR__ . '/../database.php';       // Connesione alla logica del DATABASE
+include_once __DIR__ . '/../function.php';  // Connesione a function.php
 
-include __DIR__ . '/../database.php';   // Connesione alla logica del DATABASE
-
-/**  
- * 
- * Ottenimento lista stanze nella home 
- * 
-**/
-
-$sql = "SELECT * FROM `stanze`";    // Query strin per ottenere la lista della stanze  
-$result = $conn->query($sql);
-
-/**  
- * 
- * Controllo dei risultati 
- * 
-**/
-
-if ( $result && $result->num_rows > 0 ) {
-    $rooms = [];
-    
-    // Loop per estrapolare le row
-    while ( $row = $result->fetch_assoc() ) { 
-        $rooms[] = $row;
-    }
-} elseif ( $result ) {
-    echo 'Nessun risultato';
-} else {
-    echo 'Errore della query';
-}
-
-// End Connection
-$conn->close();
+$rooms = getAll($conn, 'stanze');
